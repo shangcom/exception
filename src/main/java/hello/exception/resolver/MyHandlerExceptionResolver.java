89abfc8764603080ff5d77f98be58d652515c84d
shangcom <dndbs91@gmail.com>
2024-10-10 20:20:34 +0900

    섹션10
    API 예외 처리
    
    HandlerExceptionResolver 활용
    
    완료

---
+++ src/main/java/hello/exception/resolver/MyHandlerExceptionResolver.java
@@ -0,0 +1,29 @@
+package hello.exception.resolver;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+
+@Slf4j
+public class MyHandlerExceptionResolver implements HandlerExceptionResolver {
+    @Override
+    public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+
+        log.info("call resolver= {}", ex);
+        try {
+            if (ex instanceof IllegalArgumentException) {
+                log.info("IllegalArgumentException resolver to 400");
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
+                return new ModelAndView(); // 빈 ModelAndView 객체 반환할 경우, 스프링은 view를 렌더링하지 않는다.
+            }
+        } catch (IOException e) {
+            log.error("resolver ex", e);
+        }
+
+        return null;
+    }
+}
